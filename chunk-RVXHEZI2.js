065bb2bf5e815b6803f76d65e267f097d7f1f308
Mohamed <okar4598@gmail.com>
2025-07-27 04:02:46 +0300

    Upload Route Task

---
+++ chunk-RVXHEZI2.js
@@ -0,0 +1,1 @@
+import{Lb as s,Ua as o,tb as n,ub as a}from"./chunk-5ZRHD5FN.js";var f=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-notfound"]],decls:2,vars:0,template:function(t,p){t&1&&(n(0,"p"),s(1,"notfound works!"),a())},encapsulation:2})};export{f as Notfound};
---
+++ chunk-TOSMD5I3.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-GWIRIJCF.js";import"./chunk-DO3Q7BKG.js";import"./chunk-5ZRHD5FN.js";export{a as DisplayProducts};
